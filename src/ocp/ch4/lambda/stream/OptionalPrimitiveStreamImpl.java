d2fc49f5628f3da06d134e0ba4641a29e08139a1
Hsu Wenche <hsu.wenche@outlook.com>
2017-01-12 17:59:29 +0800

    IntStream Statistic - P210

--- src/ocp/ch4/lambda/stream/OptionalPrimitiveStreamImpl.java
+++ src/ocp/ch4/lambda/stream/OptionalPrimitiveStreamImpl.java
@@ -5,8 +5,12 @@
  */
 package ocp.ch4.lambda.stream;
 
+import java.util.IntSummaryStatistics;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 /**
  *
@@ -20,5 +24,30 @@ public class OptionalPrimitiveStreamImpl {
         IntStream intStream = IntStream.rangeClosed(1, 10);
         OptionalDouble optional = intStream.average();
         optional.ifPresent(System.out::println);
+        System.out.println("optional = " + optional.getAsDouble());
+        System.out.println("optional = " + optional.orElseGet(() -> Double.NaN));
+        // Optional type primitive
+        System.out.println("【Optional type primitive】");
+        LongStream longs = LongStream.of(5, 10);
+        long sum = longs.sum();
+        System.out.println("sum = " + sum);
+        DoubleStream doubles = DoubleStream.generate(() -> Math.PI);
+        OptionalDouble min = doubles.limit(10).min();
+        System.out.println("min = " + min.getAsDouble());
+    }
+
+    // Statistic summary (orElseThrow())
+    private static int max(IntStream ints) {
+        OptionalInt optional = ints.max();
+        return optional.orElseThrow(RuntimeException::new);
+    }
+
+    // Statistic summary (IntSummaryStatistics, getMax(), getMin())
+    private static int range(IntStream ints) {
+        IntSummaryStatistics stats = ints.summaryStatistics();
+        if (stats.getCount() == 0) {
+            throw new RuntimeException();
+        }
+        return stats.getMax() - stats.getMin();
     }
 }
