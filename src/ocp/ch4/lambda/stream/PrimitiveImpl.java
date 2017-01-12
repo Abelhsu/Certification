b9f491049ee8f12a37d691e7ebf162d54282fd49
Hsu Wenche <hsu.wenche@outlook.com>
2017-01-12 13:25:20 +0800

    Primitives - P205

--- src/ocp/ch4/lambda/stream/PrimitiveImpl.java
+++ src/ocp/ch4/lambda/stream/PrimitiveImpl.java
@@ -5,23 +5,20 @@
  */
 package ocp.ch4.lambda.stream;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Stream;
 
 /**
  *
  * @author 10405
  */
-public class PuttingTogetherImpl {
+public class PrimitiveImpl {
 
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("Toby", "Anna", "Leroy", "Alex");
-        list.stream()
-                .filter(n -> n.length() == 4)
-                .sorted().limit(2)
-                .forEach(System.out::println);
+        // Primitive
+        System.out.println("【Primitive】");
         Stream<Integer> stream = Stream.of(1, 2, 3);
-        System.out.println(stream.reduce(0, (s, n) -> s + n));
+        System.out.println("reduce(): " + stream.reduce(0, (s, n) -> s + n));
+        stream = Stream.of(1, 2, 3);
+        System.out.println("mapToInt(): "+stream.mapToInt(x -> x).sum());
     }
 }
